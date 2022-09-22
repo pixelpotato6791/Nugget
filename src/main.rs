bcdc49d522cecc06e82d5b971d521d10c54abcb9
PixelPotato <pixelpotato.6791@outlook.com>
2022-09-22 16:04:06 +0100

    GTK hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
+const APP_ID: &str = "org.pixelpotato.nugget";
+
 fn main() {
-    println!("Hello this is nugget");
+    let app = Application::builder()
+        .application_id(APP_ID)
+        .build();
+
+    app.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("Hello, World!")
+            .build();
+
+        window.show();
+    });
+
+    app.run();
 }
